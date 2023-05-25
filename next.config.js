95f2a9523f0a4d19615868c4bccb7a2bb39d3b6f
m1m0zzz <contact.m1m0zzz@gmail.com>
2023-05-25 13:29:34 +0900

    use future actions

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
-  // Optional: Change the output directory `out` -> `dist`
-  distDir: 'dist',
+  output: 'export'
 }
 
 module.exports = nextConfig
